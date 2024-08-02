38da78d189a8eff6ec7d4947395ee87c745d7617
Umesh <umeshtamang129@gmail.com>
2024-08-02 19:10:23 +0545

    Category and Product design

--- website/ecomapp/views.py
+++ website/ecomapp/views.py
@@ -1,20 +1,52 @@
 from django.shortcuts import render
 from django.views.generic import View, TemplateView
+from .models import *
 
 # Create your views here.
-
+# Home Page
 class HomeView(TemplateView):
     template_name = "home.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['myname'] = "Umesh Babu Tamang "
+        context['product_list']=Product.objects.all().order_by("-id")
         return context
 
+
+# Category Show
+class AllProductsView(TemplateView):
+    template_name = "allproducts.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['allcategories'] = Category.objects.all()
+        return context
+
+
+# ProductdetailView
+class ProductDetailView(TemplateView):
+    template_name = "productdetail.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        url_slug = self.kwargs['slug']
+        product = Product.objects.get(slug=url_slug)
+        product.view_count += 1
+        product.save()
+        context['product'] = product
+        return context
+
+
+# About Page
 class AboutView(TemplateView):
     template_name = "about.html"
 
 
+# Contact Page
 class ContactView(TemplateView):
     template_name = "contact.html"
+
+
+
     
