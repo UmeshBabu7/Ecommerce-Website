96f12b6babc14f9aff54dcd8bee45da5598cc37f
Umesh <umeshtamang129@gmail.com>
2024-11-13 20:19:15 +0545

    extends template tags

--- website/ecomapp/urls.py
+++ website/ecomapp/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .import views
+from .views import *
 
 urlpatterns = [
+    path('',HomeView.as_view(),name="home"),
+    path('about/',AboutView.as_view(),name="about"),
+    path('contact/',ContactView.as_view(),name="contact")
     
 ]
+
