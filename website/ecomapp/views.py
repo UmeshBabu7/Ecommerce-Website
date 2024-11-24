eda5d59f1e60261a0f449a5b25dee23b5d40fe9d
Umesh <umeshtamang129@gmail.com>
2024-11-24 19:11:35 +0545

    Cart functionality

--- website/ecomapp/views.py
+++ website/ecomapp/views.py
@@ -34,6 +34,78 @@ class ProductDetailView(TemplateView):
         return context
 
 
+class AddToCartView(TemplateView):
+    template_name="addtocart.html"
+
+    def get_context_data(self,**kwargs):
+        context=super().get_context_data(**kwargs)
+
+    # get product id from requested url
+        product_id=self.kwargs['pro_id']
+
+    # get product
+        product_obj=Product.objects.get(id=product_id)
+     
+    # check if cart exists
+        cart_id = self.request.session.get("cart_id", None)
+        if cart_id:
+            cart_obj = Cart.objects.get(id=cart_id)
+            this_product_in_cart = cart_obj.cartproduct_set.filter(
+                product=product_obj)
+
+            # item already exists in cart
+            if this_product_in_cart.exists():
+                cartproduct = this_product_in_cart.last()
+                cartproduct.quantity += 1
+                cartproduct.subtotal += product_obj.selling_price
+                cartproduct.save()
+                cart_obj.total += product_obj.selling_price
+                cart_obj.save()
+            # new item is added in cart
+            else:
+                cartproduct = CartProduct.objects.create(
+                    cart=cart_obj, product=product_obj, rate=product_obj.selling_price, quantity=1, subtotal=product_obj.selling_price)
+                cart_obj.total += product_obj.selling_price
+                cart_obj.save()
+
+        else:
+            cart_obj = Cart.objects.create(total=0)
+            self.request.session['cart_id'] = cart_obj.id
+            cartproduct = CartProduct.objects.create(
+                cart=cart_obj, product=product_obj, rate=product_obj.selling_price, quantity=1, subtotal=product_obj.selling_price)
+            cart_obj.total += product_obj.selling_price
+            cart_obj.save()
+
+        return context
+    
+class MyCartView(TemplateView):
+      template_name = "mycart.html"
+
+      def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        cart_id = self.request.session.get("cart_id", None)
+        if cart_id:
+            cart = Cart.objects.get(id=cart_id)
+        else:
+            cart = None
+        context['cart'] = cart
+        return context
+
+
+
+
+
+
+       
+
+        
+
+
+
+
+
+
+
 class AboutView(TemplateView):
     template_name="about.html"
 
