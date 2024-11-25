992db423619d5434a3b084d9ed3ef9bbf07f75d1
Umesh <umeshtamang129@gmail.com>
2024-11-25 08:14:40 +0545

    Manage cart items

--- website/ecomapp/views.py
+++ website/ecomapp/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.shortcuts import render,redirect
+from django.views.generic import TemplateView,View
 from .models import *
 
 # Create your views here.
@@ -90,6 +90,39 @@ class MyCartView(TemplateView):
             cart = None
         context['cart'] = cart
         return context
+      
+
+class ManageCartView(View):
+    def get(self,request,*args,**kwargs):
+        cp_id=self.kwargs['cp_id']
+        action=request.GET.get('action')
+        cp_obj=CartProduct.objects.get(id=cp_id)
+        cart_obj=cp_obj.cart
+
+        if action == "inc":
+            cp_obj.quantity += 1
+            cp_obj.subtotal += cp_obj.rate
+            cp_obj.save()
+            cart_obj.total += cp_obj.rate
+            cart_obj.save()
+        elif action == "dcr":
+            cp_obj.quantity -= 1
+            cp_obj.subtotal -= cp_obj.rate
+            cp_obj.save()
+            cart_obj.total -= cp_obj.rate
+            cart_obj.save()
+            if cp_obj.quantity == 0:
+                cp_obj.delete()
+
+        elif action == "rmv":
+            cart_obj.total -= cp_obj.subtotal
+            cart_obj.save()
+            cp_obj.delete()
+        else:
+            pass
+        return redirect("ecomapp:mycart")
+
+
 
 
 
